0974b866ee06f6fcb0e6f8a869672285767ce11c
carlosValerio5 <carlitomanuelvalri@hotmail.com>
2025-04-23 22:18:58 -0600

    erase unused directory

--- premierstats_back/src/test/java/com/pl/premierstats/PremierStatsApplicationTests.java
+++ premierstats_back/src/test/java/com/pl/premierstats/PremierStatsApplicationTests.java
@@ -3,6 +3,17 @@ package com.pl.premierstats;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Pending:
+ *
+ * 1. Add tests for the controller (player and team)
+ * 2. Add tests for the service (player and team)
+ * 3. Add tests for the repository (player and team)
+ * 4. Add tests for the mapper (player)
+ * 5. Add tests for the PlayerDTO
+ *
+ *
+ */
 @SpringBootTest
 class PremierStatsApplicationTests {
 
