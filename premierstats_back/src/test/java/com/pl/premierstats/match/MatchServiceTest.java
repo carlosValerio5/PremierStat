a6f000cc80c17751aac060a27e5306d74b24194d
carlosValerio5 <carlitomanuelvalri@hotmail.com>
2025-05-04 11:48:27 -0600

    Added crossorigin functionality and added httpclient5 dependency

--- premierstats_back/src/test/java/com/pl/premierstats/match/MatchServiceTest.java
+++ premierstats_back/src/test/java/com/pl/premierstats/match/MatchServiceTest.java
@@ -1,4 +1,23 @@
 package com.pl.premierstats.match;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class MatchServiceTest {
+
+    @Autowired
+    private MatchService matchService;
+
+    @Test
+    public void getMatchesTest(){
+        assert !matchService.getMatches().isEmpty();
+    }
+
+    @Test
+    public void getMatchByIdTest() throws MatchNotFoundException {
+        assert matchService.getMatchById(1)
+    }
+
 }
